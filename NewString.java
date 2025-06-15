5e1b5df3b71463ea425793682beb41c9c1265a83
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-06-15 23:13:34 +0530

    string

---
+++ NewString.java
@@ -0,0 +1,16 @@
+// Java Program to demonstrate String
+public class NewString {
+
+   
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String("Virendra");
+        String  str1="rohit";
+        str1="hello";
+        str="v";
+
+        System.out.println(str);
+    }
+}
