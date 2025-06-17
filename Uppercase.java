26c5bb41d43b40322012462597b7a5074cbeceee
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-06-17 23:53:54 +0530

    uppercase

---
+++ Uppercase.java
@@ -0,0 +1,12 @@
+public class Uppercase {
+    static void uppercase(String name){
+        String uppername=name.toUpperCase();
+        System.out.println(uppername);
+    }
+    public static void main(String[] args) {
+        String name="virendra";
+        uppercase(name);
+        
+    }
+    
+}
